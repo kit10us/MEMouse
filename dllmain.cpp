cadc060c3c714ed478e9b26079e100b0f9afb8e0
Stacy Smith <stacy@kit10.us>
2024-06-07 13:57:14 -0700

    Updated for portability.

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,6 +1,5 @@
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-#include <windows.h>
+#include <port/win/Windows.h>
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
